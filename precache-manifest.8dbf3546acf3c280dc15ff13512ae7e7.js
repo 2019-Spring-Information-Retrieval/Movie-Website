a37cb59ee8adbe0b993e838a45bcd0c3dc9a153b
Wade <liuweixi0819@gmail.com>
2019-03-25 10:18:58 -0400

    update

--- precache-manifest.8dbf3546acf3c280dc15ff13512ae7e7.js
+++ precache-manifest.8dbf3546acf3c280dc15ff13512ae7e7.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "3715fa244f53718f1c07",
+    "revision": "a69e99db49f0ebefc3ba",
     "url": "/Movie-Website/static/css/main.c456e841.chunk.css"
   },
   {
-    "revision": "3715fa244f53718f1c07",
-    "url": "/Movie-Website/static/js/main.aa0f3ee5.chunk.js"
+    "revision": "a69e99db49f0ebefc3ba",
+    "url": "/Movie-Website/static/js/main.c22459ed.chunk.js"
   },
   {
     "revision": "242c057c28998c4d193f",
     "url": "/Movie-Website/static/js/runtime~main.d9f52f51.js"
   },
   {
-    "revision": "e31ea4425e40c80d77ea",
+    "revision": "0c9162c44d443a6d097e",
     "url": "/Movie-Website/static/css/2.1c330e54.chunk.css"
   },
   {
-    "revision": "e31ea4425e40c80d77ea",
-    "url": "/Movie-Website/static/js/2.7534f985.chunk.js"
+    "revision": "0c9162c44d443a6d097e",
+    "url": "/Movie-Website/static/js/2.829d9ef8.chunk.js"
   },
   {
-    "revision": "05aa870c2a29c18623c4d760b7732733",
+    "revision": "74058b0edc6fac6f0f130a9fa002c818",
     "url": "/Movie-Website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Movie-Website/precache-manifest.03ca385cda8ea91ac12453a7241a5ee1.js"
+  "/Movie-Website/precache-manifest.8dbf3546acf3c280dc15ff13512ae7e7.js"
 );
 
 workbox.clientsClaim();
