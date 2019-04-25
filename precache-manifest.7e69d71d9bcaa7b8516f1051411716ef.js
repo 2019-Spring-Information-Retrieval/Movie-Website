634129e9bd36a33357de4aee789c5076feeeed55
Wade <liuweixi0819@gmail.com>
2019-04-25 01:37:22 -0400

    add trailer

---
+++ precache-manifest.7e69d71d9bcaa7b8516f1051411716ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "242c057c28998c4d193f",
+    "url": "/Movie-Website/static/js/runtime~main.d9f52f51.js"
+  },
+  {
+    "revision": "ad7e3d1abf3d9de95229",
+    "url": "/Movie-Website/static/js/main.e5eebe0d.chunk.js"
+  },
+  {
+    "revision": "5274feec639cbf553fc8",
+    "url": "/Movie-Website/static/js/2.480de86e.chunk.js"
+  },
+  {
+    "revision": "ad7e3d1abf3d9de95229",
+    "url": "/Movie-Website/static/css/main.230fb82b.chunk.css"
+  },
+  {
+    "revision": "5274feec639cbf553fc8",
+    "url": "/Movie-Website/static/css/2.a60e5345.chunk.css"
+  },
+  {
+    "revision": "3bc54840def47f2ac5e9c9473abe5837",
+    "url": "/Movie-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Movie-Website/precache-manifest.3b73975bd9788751c7477bfcd0fd2b23.js"
+  "/Movie-Website/precache-manifest.7e69d71d9bcaa7b8516f1051411716ef.js"
 );
 
 workbox.clientsClaim();
